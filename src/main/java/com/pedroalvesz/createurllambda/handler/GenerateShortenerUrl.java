96c63432cf26cebcab234109b7f5cfd703ecccda
Pedro Henrique <opedrohenriqu@gmail.com>
2025-02-12 19:06:49 -0300

    refactor: use ObjectMapperUtils to simplify GenerateShortenerUrl logics

---
+++ src/main/java/com/pedroalvesz/createurllambda/handler/GenerateShortenerUrl.java
@@ -0,0 +1,26 @@
+package com.pedroalvesz.createurllambda.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.pedroalvesz.createurllambda.UrlDataDTO;
+import com.pedroalvesz.createurllambda.manager.S3ManagerService;
+import com.pedroalvesz.createurllambda.utils.ObjectMapperUtils;
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class GenerateShortenerUrl implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private final S3ManagerService s3ManagerService;
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = input.get("body").toString();
+        UrlDataDTO urlDataDTO = ObjectMapperUtils.fromJson(body, UrlDataDTO.class);
+
+        String shortUrlCode = s3ManagerService.putObject(urlDataDTO);
+
+        return Map.of("shortUrlCode", shortUrlCode);
+    }
+}
